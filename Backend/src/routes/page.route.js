b5193ff34c07f7037914925b69526bfb82b0d6d1
jyotibrotoRoy <144539363+JyotibrotoRoy@users.noreply.github.com>
2025-06-18 16:45:23 +0530

    changed pagerouter

--- Backend/src/routes/page.route.js
+++ Backend/src/routes/page.route.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
 
 const router = Router()
 
-router.get('/login', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'public', 'login.html'));
-}); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-router.get('/homepage', verifyJWT, (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'public', 'homepage.html'))
-})
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public', 'login.html'));
+});
+
+router.get('/homepage', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public', 'homepage.html'));
+});
 
 router.get('/', (req, res) => {
     res.redirect('/login');
