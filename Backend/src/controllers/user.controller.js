573a830a938bff3e343a0818c2bf930789b7e381
jyotibrotoRoy <144539363+JyotibrotoRoy@users.noreply.github.com>
2025-06-18 15:48:11 +0530

    fix: Correct import path for ApiError to ensure proper module resolution

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "utils/apiError.js";
+import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.models.js"
 import { ApiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
